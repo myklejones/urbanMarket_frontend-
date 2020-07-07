773a197594c81c12c0e5c681b8ea97231216554b
mykle jones <mykle.j91@gmail.com>
2020-07-06 20:52:59 -0400

    products page

--- src/component/Products.js
+++ src/component/Products.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Products() {
     return (
         <div>
-            
+            <h1>This is the products page 
+            </h1>
         </div>
     )
 }
