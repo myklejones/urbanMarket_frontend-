a5c85c1b52ae82d083c3a369d957da78e57ce226
mykle jones <mykle.j91@gmail.com>
2020-07-11 18:08:44 -0400

    redirect page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import { Route, Switch} from 'react-router'
 import Login from './component/Login'
 import Home from './component/Home'
+import Redirect from './component/Redirect'
 
 function App() {
   return (
    
     <Switch>
+      <Route exact path="/home"   render={(routerProps)=><Home {...routerProps}  /> }    />\
+      <Route exact path="*"   render={(routerProps)=><Redirect {...routerProps}  /> }    />
       <Route path="/login"   render={(routerProps)=><Login {...routerProps}  /> }    />
-      <Route exact path="*"   render={(routerProps)=><Home {...routerProps}  /> }    />
       </Switch>
   );
 }
