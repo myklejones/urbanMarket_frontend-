dc78bbaa748e2e1d64990e7d8e152f78bd2870f3
mykle jones <mykle.j91@gmail.com>
2020-07-12 22:48:14 -0400

    imput on form working

--- src/component/CreateUser.js
+++ src/component/CreateUser.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import {Form, Button} from 'react-bootstrap'
+
 
 const Styles = styled.div`
 
@@ -8,14 +9,39 @@ const Styles = styled.div`
 
 
 function NewUser() {
-    return (
-      <Styles>
 
+  const [email, setEmail]= useState('')
+  const [password, setPassword]= useState('')
+
+
+  const formInput = evt => {
+
+    console.log(evt.target.name)
+    switch(evt.target.name){
+      case 'email':
+        setEmail(evt.target.value)
+        break;
+      case 'password':
+        setPassword(evt.target.value)
+        break;  
+    }
+    // setInput(evt.target.value)
+  }
+
+
+
+
+
+
+  console.log(email, password)
+  console.log("create user page")
+    return (
+      <div className="container" >
     
-      <Form>
+      <Form >
       <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Label className="mt-5">Email address</Form.Label>
+        <Form.Control name="email" type="email" placeholder="Enter email" value={email} onChange={formInput}  />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -23,7 +49,7 @@ function NewUser() {
     
       <Form.Group controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control name="password" type="password" placeholder="Password" value={password} onChange={formInput} />
       </Form.Group>
       <Form.Group controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
@@ -32,7 +58,7 @@ function NewUser() {
         Submit
       </Button>
     </Form>
-    </Styles>
+    </div >
             
        
     )
