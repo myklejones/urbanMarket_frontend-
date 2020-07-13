132c33d445084e376a0dd883f55428bafb2848d5
mykle jones <mykle.j91@gmail.com>
2020-07-12 22:08:19 -0400

    new user form

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { Route, Switch} from 'react-router'
 import Login from './component/Login'
 import Home from './component/Home'
 import Redirect from './component/Redirect'
+import CreateUser from './component/CreateUser'
 
 function App() {
   return (
    
     <Switch>
-      <Route exact path="/home"   render={(routerProps)=><Home {...routerProps}  /> }    />\
+      <Route exact path="/home"   render={(routerProps)=><Home {...routerProps}  /> }    />
       <Route path="/login"   render={(routerProps)=><Login {...routerProps}  /> }    />
+      <Route path="/createUser"   render={(routerProps)=><CreateUser {...routerProps}  /> }    />
       <Route exact path="*"   render={(routerProps)=><Redirect {...routerProps}  /> }    />
       </Switch>
   );
