8157b7d49c16386002a8e296a6dd070843156abe
mykle jones <mykle.j91@gmail.com>
2020-07-15 20:25:03 -0400

    login form '

--- src/component/login.js
+++ src/component/login.js
@@ -3,7 +3,26 @@ import React from 'react'
 function login() {
     return (
         <div>
-            <h1>login page</h1>
+              <Form >
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label className="mt-5">Email address</Form.Label>
+        <Form.Control name="email" type="email" placeholder="Enter email" value={email} onChange={formInput}  />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+    
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control name="password" type="password" placeholder="Password" value={password} onChange={formInput} />
+      </Form.Group>
+      <Form.Group controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
             
         </div>
     )
